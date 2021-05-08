5d38b6c5ba7f458c0b97e7709b81750d9f7fe31c
Isak Pettersson <isakpetterssonbusiness@gmail.com>
2021-05-08 21:18:10 +0200

    Sass added to webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import './index.css';
+import './style.scss';
 
 ReactDOM.render(<h1>Do your magic</h1>, document.getElementById('root'));
